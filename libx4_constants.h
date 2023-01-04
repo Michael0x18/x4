4e7ca9ad529bc71007f8e0e4fa1dc8414cd324b7
Michael0x18 <michaelsunn101@gmail.com>
2023-01-03 22:15:40 -0800

    Refactor more

--- libx4_constants.h
+++ libx4_constants.h
@@ -2,9 +2,11 @@
 #define libx4_constants_h
 
 #define X4_VRAM (void *)0xD40000
-#define X4_BUFFER_1 (void *)0xD40000
-#define X4_BUFFER_2 (void *)(0xD40000 + 38400)
-#define X4_BUFFER_3 (void *)(0xD40000 + 76800)
-#define X4_BUFFER_4 (void *)(0xD40000 + 115200)
+#define X4_BUFFER_0 (void *)0xD40000
+#define X4_BUFFER_1 (void *)(0xD40000 + 38400)
+#define X4_BUFFER_2 (void *)(0xD40000 + 76800)
+#define X4_BUFFER_3 (void *)(0xD40000 + 115200)
+
+#define X4_fixed_point_factor 64
 
 #endif
